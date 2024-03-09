785f3106f4ebbda1cdfbee8c871bd13be4733a2d
owenfisher <owenfisher46@gmail.com>
2024-03-08 16:12:29 -0800

    - renamed a couple user options to make it more clear

--- src/main/persistence/Writeable.java
+++ src/main/persistence/Writeable.java
@@ -5,7 +5,7 @@ import org.json.JSONObject;
 
 //this persistance package is heaviliy inspired by the example given to us
 
-//contains toJson method that the model classes must implement
+//contains abstract toJson method that the model classes must implement
 public interface Writeable {
 
     // EFFECTS: returns this as JSON object
